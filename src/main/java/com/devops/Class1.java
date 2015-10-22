42eeb3d0d7424aae8aab36379ba7604f3bddd01d
Arvind <adtelhar@ncsu.edu>
2015-10-22 02:48:18 -0400

    Adding 2 more classes to test scripts

---
+++ src/main/java/com/devops/Class1.java
@@ -0,0 +1,9 @@
+public class Class1
+{
+//This is just a test class
+//No use as such
+ public static void main(String args[]){
+  System.out.println("This is class1");
+ }
+}
+
---
+++ src/main/java/com/devops/Class2.java
@@ -0,0 +1,9 @@
+public class Class2
+{
+//This is just a test class
+//No use as such
+ public static void main(String args[]){
+  System.out.println("This is class2");
+ }
+}
+
