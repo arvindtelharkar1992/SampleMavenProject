b20a308580b122657d6155d4349826c2166979fe
Arvind <adtelhar@ncsu.edu>
2015-10-22 01:55:30 -0400

    Adding 2 comments

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -6,8 +6,10 @@ package com.devops;
  */
 public class App 
 {
+  //This is a App Class generated during the maven project Creation
     public static void main( String[] args )
     {
+      //This is the main function
         System.out.println( "Hello World!" );
         System.out.println("This is a change");
     }
